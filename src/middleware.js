6aa881a7cd8fa023c863d11d51c24e6cdba5ad3b
zereight <duaxka7@naver.com>
2019-11-21 15:07:53 +0900

    View Login or not & bug fix

--- src/middleware.js
+++ src/middleware.js
@@ -4,5 +4,12 @@ import {routers} from "./routes";
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "YouTube";
   res.locals.routes = routers;
+
+  res.locals.user= {
+    isAuthenticated: true,
+    id: 1
+  }
+  
   next();
 }
+
